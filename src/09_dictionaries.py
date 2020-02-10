26734595019e8577d3959c34b386c9353abedc15
Martins-O-U <martinsonyedikachi@gmail.com>
2020-02-10 20:27:44 +0100

    worked on tuples down to dictionary

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -4,25 +4,22 @@ There's not much different with Python's version compared to what
 you'll find in other languages (though you can also initialize and
 populate dictionaries using comprehensions just like you can with
 lists!).
-
 The docs can be found here:
 https://docs.python.org/3/tutorial/datastructures.html#dictionaries
-
 For this exercise, you have a list of dictionaries. Each dictionary
 has the following keys:
  - lat: a signed integer representing a latitude value
  - lon: a signed integer representing a longitude value
  - name: a name string for this location
 """
-
 waypoints = [
     {
-        "lat": 43,
+        "lat": 41,
         "lon": -121,
         "name": "a place"
     },
     {
-        "lat": 41,
+        "lat": 42,
         "lon": -123,
         "name": "another place"
     },
@@ -36,12 +33,20 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+waypoints.append({"lat": 44,"lon": -133,"name": "a nice place"})
+print(waypoints)
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
-# Note: It's okay to access the dictionary using bracket notation on the
-# waypoints list.
-
 # YOUR CODE HERE
+
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = 'not a real place'
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
+# YOUR CODE HERE 
 # YOUR CODE HERE
+
+for i in waypoints:
+    print(i["lat"], i["lon"], i['name'])
